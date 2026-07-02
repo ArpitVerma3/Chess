5f012f7a5f7798f02801135e881c7239b9642f3b
ArpitVerma3 <varpit520@gmail.com>
2026-07-03 00:56:40 +0530

    addingCloak

--- Server/socket.js
+++ Server/socket.js
@@ -7,28 +7,30 @@ const io = new Server(httpServer, {
   cors: {
     origin: ["http://127.0.0.1:5500", "http://localhost:5500"],
     allowedHeaders: ["GET", "POST"],
-    credentials: true
-  }
+    credentials: true,
+  },
 });
 
-let totalPlayers=0;
+let totalPlayers = 0;
+let players = {};
 
-function onDisconnect(socket){
-    totalPlayers--;
-    firePlayers();
+function onDisconnect(socket) {
+  totalPlayers--;
+  firePlayers();
 }
-function firePlayers(){
-    io.emit('total_players_count_change', totalPlayers);
+function firePlayers() {
+  io.emit("total_players_count_change", totalPlayers);
 }
-function fireOnConnected(){
-    totalPlayers++;
-    firePlayers();
+function fireOnConnected() {
+  totalPlayers++;
+  firePlayers();
 }
 io.on("connection", (socket) => {
+  players[socket.id] = socket;
   totalPlayers++;
   fireOnConnected(socket);
 
-  socket.on('disconnect',()=> onDisconnect(socket))
+  socket.on("disconnect", () => onDisconnect(socket));
 });
 
 httpServer.listen(PORT);
